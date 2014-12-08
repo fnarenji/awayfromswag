984c08af3e7e8edf9b50df97487d565044dee5d5
loick111 <loick111@gmail.com>
2014-12-08 11:26:42 +0100

    init

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loick
+ * Date: 08/12/14
+ * Time: 11:18
+ */ 
---
+++ system/mvc/Model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loick
+ * Date: 08/12/14
+ * Time: 11:24
+ */
+
+namespace system\mvc;
+
+
+class Model {
+
+} 
