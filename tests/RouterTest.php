b61fa2f8fc2ac30da9ffac7ec72808e2f220fcb2
Thomas Munoz <thomas.munoz30@gmail.com>
2014-12-26 19:07:26 +0100

    Create Test directory
    The real code will be available tonight.

---
+++ tests/RouterTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomasmunoz
+ * Date: 26/12/14
+ * Time: 19:06
+ */
+
+namespace tests;
+
+
+class RouterTest extends \PHPUnit_Framework_TestCase {
+
+}
