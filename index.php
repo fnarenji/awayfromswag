5e79662bb70f7eb032c91f1d53ca86333aaee442
loick111 <loick111@gmail.com>
2014-12-29 17:56:37 +0100

    add defines & debug

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: loick
- * Date: 08/12/14
- * Time: 11:18
- */ 
+
+define(DS, DIRECTORY_SEPARATOR);
+define(ROOT, __DIR__ . DS);
+define(WEBROOT, '/');
+
+define(DEBUG, true);
+
+if(DEBUG) {
+    ini_set('display_errors', true);
+    ini_set('html_errors', true);
+    error_reporting(E_ALL);
+}
