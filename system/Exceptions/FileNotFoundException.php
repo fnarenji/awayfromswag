ef0830727cd3c7eb6403cc0c93464ecf1744093b
loick111 <loick111@gmail.com>
2014-12-29 17:54:49 +0100

    add exceptions

---
+++ system/Exceptions/FileNotFoundException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomasmunoz
+ * Date: 27/12/14
+ * Time: 23:19
+ */
+
+namespace SwagFramework\Exceptions;
+
+
+class FileNotFoundException extends \Exception
+{
+    private $previous;
+
+    public function __construct($file, $code = 0, Exception $previous = null)
+    {
+        $message = 'The file ' . $file . ' was not found !';
+
+        parent::__construct($message, $code);
+
+        if (!is_null($previous))
+            $this->previous = $previous;
+    }
+}
---
+++ system/Exceptions/HelperAlreadyExistsException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomasmunoz
+ * Date: 27/12/14
+ * Time: 23:19
+ */
+
+namespace SwagFramework\Exceptions;
+
+
+class HelperAlreadyExistsException extends \Exception
+{
+    private $previous;
+
+    public function __construct($helper, $code = 0, Exception $previous = null)
+    {
+        $message = 'The helper ' . $helper . ' already exists !';
+
+        parent::__construct($message, $code);
+
+        if (!is_null($previous))
+            $this->previous = $previous;
+    }
+}
