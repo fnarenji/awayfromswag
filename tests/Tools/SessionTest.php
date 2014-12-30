84a201d56bab32ae0b632bd291b8ed4c3b840e3e
loick111 <loick111@gmail.com>
2014-12-30 21:54:01 +0100

    add session

---
+++ tests/Tools/SessionTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loick
+ * Date: 30/12/14
+ * Time: 21:04
+ */
+
+namespace tests\Tools;
+
+
+use SwagFramework\Tools\Session;
+
+class SessionTest extends \PHPUnit_Framework_TestCase {
+    private $session;
+
+    function __construct()
+    {
+        $this->session = new Session();
+    }
+
+    public function testSession()
+    {
+        // STRING
+        $this->session->set('test', 'testValue');
+        $this->assertEquals($this->session->get('test'), 'testValue');
+
+        // ARRAY
+        $this->session->set('test', array(
+            'test'      =>      'testvalue'
+        ));
+        $this->assertEquals($this->session->get('test'), array(
+            'test'      =>      'testvalue'
+        ));
+    }
+}
