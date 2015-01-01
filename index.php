3f7a422401d955b46a88279f681cc03ba8af4c5e
loick111 <loick111@gmail.com>
2015-01-01 22:15:54 +0100

    run router

--- index.php
+++ index.php
@@ -13,3 +13,17 @@ if(DEBUG) {
 }
 
 require 'vendor/autoload.php';
+
+try {
+    $router = new \SwagFramework\Routing\Router();
+
+    $router->add('/', new \app\controllers\TestsController(), 'index');
+
+    $router->matchCurrentRequest();
+} catch (\SwagFramework\Exceptions\SwagException $e) {
+    echo '<h1>SwagException !</h1>';
+    echo '<p>' . $e->getMessage() . '</p>';
+} catch (Exception $e) {
+    echo '<h1>Exception !</h1>';
+    echo $e;
+}
