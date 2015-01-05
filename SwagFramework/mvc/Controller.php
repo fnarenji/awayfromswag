9093341ed4190a68a611fb6b2a8a76e2d38fffb9
loick111 <loick111@gmail.com>
2015-01-05 11:04:22 +0100

    add helpers in Controller

--- SwagFramework/mvc/Controller.php
+++ SwagFramework/mvc/Controller.php
@@ -9,16 +9,24 @@
 namespace SwagFramework\mvc;
 
 
+use SwagFramework\Helpers\Input;
+use SwagFramework\Helpers\Popup;
+
 class Controller
 {
     private $loader;
     private $view;
     private $params;
+    public $helpers;
 
     function __construct()
     {
         $this->loader = new \Twig_Loader_Filesystem('app/views');
         $this->view = new View($this->loader);
+
+        $this->helpers = new \stdClass();
+        $this->helpers->input = new Input();
+        $this->helpers->popup = new Popup();
     }
 
     /**
