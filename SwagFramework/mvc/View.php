f62dfe8078ee8def77aeecfaa66d5e581afeea0e
loick111 <loick111@gmail.com>
2015-01-05 11:21:55 +0100

    update ViewHelpers

--- SwagFramework/mvc/View.php
+++ SwagFramework/mvc/View.php
@@ -11,9 +11,41 @@ namespace SwagFramework\mvc;
 
 use SwagFramework\Helpers\Assets;
 use SwagFramework\Helpers\Form;
+use SwagFramework\Helpers\Popup;
+use Twig_LoaderInterface;
+
+class ViewHelpers {
+    /**
+     * @var \SwagFramework\Helpers\Assets
+     */
+    public $assets;
+    /**
+     * @var \SwagFramework\Helpers\Form
+     */
+    public $form;
+    /**
+     * @var \SwagFramework\Helpers\Popup
+     */
+    public $popup;
+
+    function __construct()
+    {
+        $this->assets = new Assets();
+        $this->form = new Form();
+        $this->popup = new Popup();
+    }
+}
 
 class View extends \Twig_Environment
 {
+    private $helpers;
+
+    public function __construct(Twig_LoaderInterface $loader = null, $options = array())
+    {
+        parent::__construct($loader, $options);
+        $this->helpers = new ViewHelpers();
+    }
+
     public function loadTemplate($name, $index = null)
     {
         $name = $name . '.twig';
@@ -22,12 +54,8 @@ class View extends \Twig_Environment
 
     public function render($name, array $context = array())
     {
-        $helpers = new \stdClass();
-        $helpers->assets = new Assets();
-        $helpers->form = new Form();
-
         echo parent::render($name, array_merge(array(
-            'helpers'       =>      $helpers
+            'helpers'       =>      $this->helpers
         ), $context));
     }
 } 
