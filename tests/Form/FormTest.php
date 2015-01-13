23fc23b29fd0374881f259aae29affd5f02ad319
loick111 <loick111@gmail.com>
2015-01-13 19:07:27 +0100

    add FormTest

---
+++ tests/Form/FormTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loick
+ * Date: 13/01/15
+ * Time: 18:47
+ */
+
+namespace tests\Form;
+
+
+use SwagFramework\Form\Field\Input;
+use SwagFramework\Form\Field\Label;
+use SwagFramework\Form\Form;
+
+define('CR', "\n");
+define('TAB', "    ");
+
+class FormTest extends \PHPUnit_Framework_TestCase {
+
+    private $form;
+
+    private function create()
+    {
+        $this->form = new Form();
+        $this->form->setAction('test');
+        $this->form->addField(new Input('test1'));
+        $this->assertEquals('<form method="POST" action="test">
+    <input name="test1" />
+</form>', $this->form->getFormHTML());
+    }
+
+    private function createWithLabel()
+    {
+        $this->form = new Form();
+        $this->form->setAction('test');
+        $this->form->addField(new Input('test1'));
+        $this->assertEquals('<form method="POST" action="test">
+    <label for="test1">Test1</label>
+    <input name="test1" />
+</form>', $this->form->getFormHTML(array('test1' => 'Test1')));
+    }
+
+    public function testForm()
+    {
+        $this->create();
+        $this->createWithLabel();
+    }
+}
---
+++ tests/Form/InputTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loick
+ * Date: 13/01/15
+ * Time: 18:50
+ */
+
+namespace tests\Form;
+
+
+use SwagFramework\Form\Field\Input;
+
+class InputTest extends \PHPUnit_Framework_TestCase {
+
+    private $input;
+
+    private function InputEmpty()
+    {
+        $this->input = new Input('testEmpty');
+        $this->assertEquals('<input name="testEmpty" />', $this->input->getHTML());
+    }
+
+    private function InputTest()
+    {
+        $this->input = new Input('testInput');
+        $this->input->addAttribute('test', 'testValue');
+        $this->assertEquals('<input name="testInput" test="testValue" />', $this->input->getHTML());
+    }
+
+    public function testInput()
+    {
+        $this->InputEmpty();
+        $this->InputTest();
+    }
+
+}
