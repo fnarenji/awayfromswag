7f1d1402c3586c919252392e3f3efc2f6a6f13a5
loick111 <loick111@gmail.com>
2015-01-13 17:27:26 +0100

    update Form

---
+++ SwagFramework/Form/Form.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loick
+ * Date: 13/01/15
+ * Time: 15:34
+ */
+
+namespace SwagFramework\Form;
+
+
+use SwagFramework\Form\Field\Label;
+use SwagFramework\Helpers\Input;
+
+class Form
+{
+    private $method;
+    private $action;
+    private $fields = array();
+    private $input;
+
+    function __construct()
+    {
+        $this->input = new Input();
+        $this->method = 'POST';
+    }
+
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    public function setMethod($method)
+    {
+        $this->method = $method;
+    }
+
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    public function setAction($action)
+    {
+        $this->action = $action;
+    }
+
+    public function addField(\SwagFramework\Form\Field\Field $field)
+    {
+        $this->fields[$field->getName()] = $field;
+    }
+
+    public function getFields()
+    {
+        return $this->fields;
+    }
+
+    public function getField($name)
+    {
+        return (isset($this->fields[$name])) ? $this->fields[$name] : null;
+    }
+
+    public function getForm()
+    {
+        $form = array();
+
+        foreach ($this->fields as $field) {
+            $form[$field->getName()] = $this->input->post($field->getName());
+        }
+
+        return $form;
+    }
+
+    public function getFormHTML($labels = array())
+    {
+        $form = '<form '
+            . 'method="' . $this->getMethod() . '"'
+            . ' '
+            . 'action="' . $this->getAction() .'"'
+            . '>';
+
+        foreach($this->getFields() as $field)
+        {
+            if(!empty($labels) && array_key_exists($field->getName(), $labels))
+            {
+                $label = new Label($field->getName(), $labels[$field->getName()]);
+                $form .= CR . TAB . $label->getHTML();
+            }
+            $form .= CR . TAB . $field->getHTML();
+        }
+
+        $form .= CR . '</form>';
+
+        return $form;
+    }
+}
