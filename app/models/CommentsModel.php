1be98190228eec4453baaa6b8552d2fc024bd03e
sknz <florandara@gmail.com>
2015-01-16 11:25:48 +0100

    Database

--- app/models/CommentsModel.php
+++ app/models/CommentsModel.php
@@ -35,7 +35,7 @@ class CommentsModel extends Model
     public function getCommentEvent($id)
     {
         $sql = "SELECT nameEvent,contents,userName FROM Users,commentE,Comments,event WHERE " .
-            "event_idEvent = ? AND Comments_idComments = idComments AND idUsers = participateE_Users_idUsers;";
+            "event_idEvent = ? AND Comments_idComm<ents = idComments AND idUsers = participateE_Users_idUsers;";
 
         return DatabaseProvider::connection()->execute($sql, $id);
 
@@ -53,12 +53,10 @@ class CommentsModel extends Model
      */
     public function deleteCommentEvent($id)
     {
-
         $sql = "DELETE FROM commentE WHERE idcommentE = ?";
 
         DatabaseProvider::connection()->execute($sql, $id);
 
         return true;
     }
-
 } 
