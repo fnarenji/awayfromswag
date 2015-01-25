55fff54c285593eca1c20bb4ed3e4d5b5d5bbdf7
Thomas Munoz <thomas.munoz30@gmail.com>
2015-01-25 03:22:40 +0100

    Show event on calendar click

--- app/views/user/calendarDisplay.php
+++ app/views/user/calendarDisplay.php
@@ -44,8 +44,8 @@ $dates = $date->getAll($year);
                             <?php echo $date->days[$w - 1]; ?> <?php echo $d; ?>  <?php echo $date->months[$m-1]; ?>
                         </div>
                         <ul class="events">
-                            <?php if(isset($events[$time])): foreach($events[$time] as $e): ?>
-                                <li><?php echo $e; ?></li>
+                            <?php if(isset($events[$time])): foreach($events[$time] as $key => $e): ?>
+                                <li><a href="/event/show/<?= $key ?>"><?= $e ?></a></li>
                             <?php endforeach; endif;  ?>
                         </ul>
                     </td>
